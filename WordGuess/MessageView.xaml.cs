95f842c4244795625a0f768ac3642b3e8b1b80da
sefe <sebastian.ferngren@gmail.com>
2022-02-20 19:15:43 +0100

    layout stuff

--- WordGuess/MessageView.xaml.cs
+++ WordGuess/MessageView.xaml.cs
@@ -4,6 +4,20 @@ namespace WordGuess;
 
 public partial class MessageView : ContentView
 {
+	public static readonly BindableProperty BorderColorProperty = BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(MessageView), Colors.Blue);
+	public Color BorderColor
+	{
+		get { return (Color)GetValue(BorderColorProperty); }
+		set { SetValue(BorderColorProperty, value); }
+	}
+
+	public static readonly BindableProperty FillColorProperty = BindableProperty.Create(nameof(FillColor), typeof(Color), typeof(MessageView), Colors.Blue);
+	public Color FillColor
+	{
+		get { return (Color)GetValue(FillColorProperty); }
+		set { SetValue(FillColorProperty, value); }
+	}
+
 	public static readonly BindableProperty MessageProperty = BindableProperty.Create(nameof(Message),typeof(string),typeof(MessageView),string.Empty);
 	public string Message
 	{
